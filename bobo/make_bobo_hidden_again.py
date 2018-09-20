aa805fc93b0a0e10f5c246436ab6827fd47c10e8
Nikolay Tikhonov <a.for.autism1337@gmail.com>
2018-09-20 20:35:51 +0300

    Finished hiddener

--- bobo/make_bobo_hidden_again.py
+++ bobo/make_bobo_hidden_again.py
@@ -1,6 +1,6 @@
 import os
 import random
-from shutil import copyfile
+from shutil import copyfile, rmtree
 
 path = os.getcwd() + '/bobo_town'
 bobo_path = os.getcwd() + '/bobo.txt'
@@ -8,6 +8,9 @@ bobo_path = os.getcwd() + '/bobo.txt'
 depth = random.randint(5, 10)
 width = random.randint(2, 4)
 
+def cleanup(path):
+    if os.path.exists(path) and os.path.isdir(path):
+        rmtree(path)
 
 def generate_dirs(path, depth, width):
     for i in range(width):
@@ -21,8 +24,10 @@ def plant_bobo(path, depth, width, bobo_path):
     for i in range(random.randint(1, depth)):
         bobo_home += '/' + str(random.randint(1, width - 1))
     print(bobo_home)
-    copyfile(bobo_path, bobo_home)
+    copyfile(bobo_path, bobo_home + '/bobo.txt')
 
 
+
+cleanup(path)
 generate_dirs(path, depth, width)
 plant_bobo(path, depth, width, bobo_path)
