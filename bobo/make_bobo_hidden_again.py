b0e269315e7485c7fbe312b1f68688a2bf0c9f0d
Roman Sedov <darragon-nn@yandex.ru>
2018-09-20 20:17:59 +0300

    bobo generator and searcher

---
+++ bobo/make_bobo_hidden_again.py
@@ -0,0 +1,28 @@
+import os
+import random
+from shutil import copyfile
+
+path = os.getcwd() + '/bobo_town'
+bobo_path = os.getcwd() + '/bobo.txt'
+
+depth = random.randint(5, 10)
+width = random.randint(2, 4)
+
+
+def generate_dirs(path, depth, width):
+    for i in range(width):
+        os.makedirs(path + '/' + str(i))
+        if (depth - 1 > 0):
+            generate_dirs(path + '/' + str(i), depth - 1, width)
+
+
+def plant_bobo(path, depth, width, bobo_path):
+    bobo_home = path
+    for i in range(random.randint(1, depth)):
+        bobo_home += '/' + str(random.randint(1, width - 1))
+    print(bobo_home)
+    copyfile(bobo_path, bobo_home)
+
+
+generate_dirs(path, depth, width)
+plant_bobo(path, depth, width, bobo_path)
---
+++ bobo/searcher.py
@@ -0,0 +1,22 @@
+import os
+
+path = os.getcwd() + '/bobo_town'
+
+def find_bobo_path(path, GREAT_BOBO):
+    in_dir = os.scandir(path)
+    for item in in_dir:
+        if (item.name == GREAT_BOBO):
+            return path + '/' + GREAT_BOBO
+        if (item.is_dir()):
+            new_path = path + '/' + item.name
+            result = find_bobo_path(new_path, GREAT_BOBO)
+            if result:
+                return result
+
+bobo_path = find_bobo_path(path, 'bobo.txt')
+
+if bobo_path == None:
+    print('где же этот чертов бобо')
+    exit(0)
+
+print(open(bobo_path).read())
