8c00195b67eda4afa3a7fdf0866c25d129e46e2b
Roman Sedov <darragon-nn@yandex.ru>
2018-09-29 16:14:53 +0300

    improved codestyle of bobo_searcher

--- bobo/searcher.py
+++ bobo/searcher.py
@@ -2,20 +2,22 @@ import os
 
 path = os.getcwd() + '/bobo_town'
 
+
 def find_bobo_path(path, GREAT_BOBO):
     in_dir = os.scandir(path)
     for item in in_dir:
-        if (item.name == GREAT_BOBO):
+        if item.name == GREAT_BOBO:
             return path + '/' + GREAT_BOBO
-        if (item.is_dir()):
+        if item.is_dir():
             new_path = path + '/' + item.name
             result = find_bobo_path(new_path, GREAT_BOBO)
             if result:
                 return result
 
+
 bobo_path = find_bobo_path(path, 'bobo.txt')
 
-if bobo_path == None:
+if bobo_path is None:
     print('где же этот чертов бобо')
     exit(0)
 
